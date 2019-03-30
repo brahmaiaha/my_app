beaadc3e68063a88b595b791f95c5e8dd3946201
brahmaiaha <krishnaiah.abburi@gmail.com>
2019-03-30 16:50:40 +0530

    this is my maven project

---
+++ src/main/java/com/brahma/app/App.java
@@ -0,0 +1,13 @@
+package com.brahma.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
